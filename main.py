d9f139971038ffdcc974b7031136bbfa3eade3e2
Maryam Shadloo <33298105+mshadloo@users.noreply.github.com>
2021-04-28 19:58:47 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,178 +1,105 @@
+from utils import load_data, train_test_val_split,getDataDict,MSE_observed_ratings, save_result, selu
+import argparse
+from autorec import autorec_model, one_epoch_train, evaluate
 import numpy as np
-import math
-import tensorflow as tf
-from tensorflow.keras.layers import Input, Dense, Flatten, Conv2D, MaxPooling2D, Embedding, Concatenate
-from tensorflow.keras.models import Model, Sequential
-from tensorflow.keras import backend as K
+from tensorflow.keras.activations import tanh,softplus
 import os
-from tensorflow.keras.regularizers import l2
+from default_config import activation, hidden, optimizer, data_dir, dataset, epochs, batch_size
 import matplotlib
 matplotlib.use("TkAgg")
 import matplotlib.pyplot as plt
 os.environ['KMP_DUPLICATE_LIB_OK']='True'
 
-def getDataDict(ratings):
-    #input:a ratings dataset. col1:user_id col2:item_id col3:data
-    #output: a dictionary keys: item_id value: list of two lists: one for user_id and one for data
-    ratings_dict={}
-    for i in range(ratings.shape[0]):
-        if(ratings_dict.get(ratings[i,1]) is None):
-            ratings_dict[ratings[i,1]]=[[ratings[i,0]],[ratings[i,2]]]
-        else:
-            ratings_dict[ratings[i,1]][0].append(ratings[i,0])
-            ratings_dict[ratings[i,1]][1].append(ratings[i,2])
-    return  ratings_dict
 
-def MSE_observed_ratings(r,r_pred):
-    tf_zero=tf.constant(0,dtype=tf.float32)
-    non_zero_positions=tf.not_equal(r,tf_zero)
-    r=tf.boolean_mask(r,non_zero_positions)
-    r_pred=tf.boolean_mask(r_pred,non_zero_positions)
-    return K.mean(K.square(r_pred - r), axis=-1)
+parser = argparse.ArgumentParser()
+activation_options = {'tanh':tanh, 'selu':selu, 'softplus':softplus}
+optimizer_options =['adam', 'sgd', 'RMSprop']
+parser.add_argument('--activation', '-activation', metavar='activation', default=activation,
+                    choices=list(activation_options.keys()),
+                    help='Activation function of hidden layer: ' + ' | '.join(list(activation_options.keys())) +
+                    ' (default: tanh)')
+parser.add_argument('--optimizer', '-optimizer', metavar='Optimizer', default=optimizer,choices=optimizer_options,
+                    help='Optimization method: ' +" | ".join(optimizer_options)+' (default: adam)')
+parser.add_argument('--epochs', default=epochs, type=int, metavar='N',
+                    help='number of total epochs to run')
+parser.add_argument('--hidden', default=hidden, type=int, metavar='N',
+                    help='number of hidden units of Recurrent Layer')
 
-def get_x_test_dict(x_train_dict,test_items):
-    x_test_dict={}
-    for item in test_items:
-        if (item in  x_train_dict):
-            x_test_dict[item]=x_train_dict[item]
-        else:
-            x_test_dict[item]=[[],[]]
-    return  x_test_dict
+parser.add_argument('--batch_size', default=batch_size, type=int,
+                    metavar='N', help='mini-batch size (default: 128)')
+parser.add_argument('--lr', '--learning-rate', default=0.01, type=float,
+                    metavar='LR', help='initial learning rate')
+parser.add_argument('--momentum', default=0.9, type=float, metavar='M',
+                    help='momentum')
+parser.add_argument('--weight_decay', '--wd', default=1e-4, type=float,
+                    metavar='W', help='weight decay (default: 1e-4)')
+parser.add_argument('--checkpoint', dest='checkpoint',
+                    help='The directory used to save the trained models',
+                    default='checkpoint', type=str)
+parser.add_argument('--dataset', dest='dataset',
+                    help='Name of data file ',
+                    default=dataset, type=str)
+parser.add_argument('--data_dir', dest='data_dir',
+                    help='The directory containing dataset',
+                    default=data_dir, type=str)
 
-
-def evaluate(model,x_test_dict,y_test_dict):
-    mse = 0
-    mae = 0
-    n_ratings = 0
-    batch_size=500
-    num_items = len(list(y_test_dict.keys()))
-    num_batches = int(math.ceil(num_items / batch_size))
-    num_users=model.layers[0].input_shape[1]
-    for b in range(num_batches):
-        b_size = min(batch_size, num_items - b * batch_size)
-        batch=np.zeros((b_size,num_users))
-        for i in range(b_size):
-            item_ind=list(x_test_dict.keys())[i+b*batch_size]
-            batch[i, x_test_dict[item_ind][0]] = x_test_dict[item_ind][1]
-
-        r_bacth_pred = model.predict(batch)
-
-        for i in range(b_size):
-            item_ind=list(y_test_dict.keys())[i+b*batch_size]
-            r_item=np.array([y_test_dict[item_ind][1]])
-            r_hat_item=[]
-            for u in y_test_dict[item_ind][0]:
-                if(u>=num_users):
-                    print('find new user')
-                    r_hat_item.append(0)
-                else:
-                    r_hat_item.append(r_bacth_pred[i,u])
-            r_hat_item=np.array([r_hat_item])
-            mse += np.sum((r_hat_item-r_item) ** 2)
-            mae += np.sum(np.abs(r_hat_item-r_item))
-            n_ratings += len(y_test_dict[item_ind][1])
-    return np.sqrt(mse/n_ratings),mae/n_ratings
+parser.add_argument('--result_dir', dest='result',
+                    help='The directory used to save the results',
+                    default='result', type=str)
 
 
+r_cols = ['user_id', 'movie_id', 'rating', 'unix_timestamp']
+args = parser.parse_args()
+working_dir = '.'
+
+data_dir = args.data_dir
+checkpoint_dir = os.path.join(working_dir, args.checkpoint )
+if not os.path.exists(checkpoint_dir):
+      os.makedirs(checkpoint_dir)
+result_dir = os.path.join(working_dir, args.result )
+if not os.path.exists(result_dir):
+      os.makedirs(result_dir)
 
 
-MOVIELENS_DIR = 'RatingsData'
-ratings= np.load('ratingsSets100k.npz')
-train_ratings=ratings['train_data']
-val_ratings=ratings['val_data']
-test_ratings=ratings['test_data']
-
-num_users=np.max(train_ratings[:,0])+1
+#input user ratings for ech item, item based
 
 
-y_train_dict=getDataDict(train_ratings)
-y_test_dict=getDataDict(test_ratings)
-y_val_dict=getDataDict(val_ratings)
+if __name__ == '__main__':
 
-x_train_dict=y_train_dict
-x_test_dict=get_x_test_dict(x_train_dict,list(y_test_dict.keys()))
-x_val_dict=get_x_test_dict(x_train_dict,list(y_val_dict.keys()))
-
-
-
-r_i=Input(shape=(num_users,))
-h1=Dense(50,activation='tanh',kernel_regularizer=l2(0))(r_i)
-r_hat_i=Dense(num_users,activation='linear')(h1)
-model=Model(inputs=r_i, outputs=r_hat_i)
-model.compile(optimizer='adam', loss=MSE_observed_ratings,  metrics=['mae'])
-
-
-print('model created')
-
-
-
-num_epochs=22
-batch_size=100
-train_mae = np.zeros((num_epochs,1))
-train_mse = np.zeros((num_epochs,1))
-train_score = np.zeros((num_epochs,1))
-test_mae = np.zeros((num_epochs,1))
-test_mse = np.zeros((num_epochs,1))
-test_score = np.zeros((num_epochs,1))
-val_mae = np.zeros((num_epochs,1))
-val_mse = np.zeros((num_epochs,1))
-val_score = np.zeros((num_epochs,1))
-
-
-num_items=len(x_train_dict.keys())
-num_batches=int(math.ceil(num_items/batch_size))
-
-xax = np.arange(0,num_epochs,1)
-
-fig = plt.figure()
-ax = fig.gca()
+    rating_data = load_data(os.path.join(data_dir, args.dataset), r_cols)
+    train_ratings, test_ratings, val_ratings = train_test_val_split(rating_data, test_size= 0.2)
+    train_dict = getDataDict(train_ratings)
+    test_dict = getDataDict(test_ratings)
+    val_dict = getDataDict(val_ratings)
+    ratings_dict = train_dict
+    num_users = np.max(train_ratings[:, 0]) + 1
+    model = autorec_model(num_users,args.hidden, activation_options[args.activation])
+    model.compile(optimizer=args.optimizer, loss=MSE_observed_ratings, metrics=['mae'])
+    rmse_train, mae_train = [], []
+    rmse_test, mae_test = [], []
+    best_rmse = float('inf')
+    for epoch in range(args.epochs):
+        one_epoch_train(model, train_dict, args.batch_size, num_users)
+        mse_train,mae_train_ = evaluate(model, train_dict, ratings_dict, batch_size=args.batch_size, num_users= num_users)
+        mse_test, mae_test_ = evaluate(model, test_dict, ratings_dict, batch_size=args.batch_size,
+                                        num_users=num_users)
+        print('Epoch {} train_mse {:.4f} train_mae {:.4f}'.format(epoch +1,  mse_train,mae_train_))
+        print('Epoch {} test_mse {:.4f} test_mae {:.4f}'.format(epoch + 1, mse_test, mae_test_))
+        rmse_train.append(mse_train)
+        mae_train.append(mae_train_)
+        rmse_test.append(mse_test)
+        if mse_test < best_rmse:
+            best_rmse = mse_test
+            model.save(os.path.join(checkpoint_dir, 'best_model_'+args.activation+"_"+str(args.hidden)+"_"+args.optimizer+'.h5'))
+            save_result(os.path.join(result_dir,"best_result_"+args.activation+"_"+str(args.hidden)+"_"+args.optimizer), mse_train, mae_train_, mse_test, mae_test_)
+        mae_test.append(mae_test_)
+    model.save(os.path.join(checkpoint_dir, 'model_'+args.activation+"_"+str(args.hidden)+"_"+args.optimizer+'.h5'))
+    save_result(os.path.join(result_dir,"result_"+args.activation+"_"+str(args.hidden)+"_"+args.optimizer), rmse_train, mae_train,rmse_test, mae_test)
 
 
 
 
 
 
-for epoch in range(num_epochs):
-    #evaluate all ratings: train, test and val with current model on this epoch
-    train_mse[epoch], train_mae[epoch]=evaluate(model,x_train_dict,y_train_dict)
-    test_mse[epoch], test_mae[epoch]= evaluate(model,x_test_dict,y_test_dict)
-    val_mse[epoch], val_mae[epoch] = evaluate(model,x_val_dict,y_val_dict)
-
-    #print('train_mae: ', train_mae[epoch])
-    print('train_mse: ', train_mse[epoch])
-    print('test_mse: ', test_mse[epoch])
-
-    ax.clear()
-    plt.xlim([-1,num_epochs])
-    plt.ylim([0,2])
-    plt.xlabel('number of epochs')
-    plt.ylabel('mean square error')
-    ax.plot(xax[0:epoch+1], train_mse[0:epoch+1], lw=0.5, linewidth=1, color='r', marker='o',markerfacecolor='b', label='train', linestyle='-',
-            markersize=1)
-    ax.plot(xax[0:epoch + 1], test_mse[0:epoch + 1], lw=0.5, linewidth=1, color='k', marker='o', markerfacecolor='b',
-            label='test', linestyle='-',
-            markersize=1)
-    ax.legend()
-    plt.show(block=False)
-    plt.pause(.5)
-    shuffle_items_inds = np.random.permutation(num_items)
-    for b in range(num_batches):
-        b_size=min(batch_size,num_items-b*batch_size)
-        batch = np.zeros((b_size, num_users))
-        for i in range(b_size):
-            item_ind=list(x_train_dict.keys())[shuffle_items_inds[i+b*batch_size]]
-            batch[i,x_train_dict[item_ind][0]]=x_train_dict[item_ind][1]
-        #print("batch "+ str(b) +" is made")
-        model.fit(batch, batch, epochs=1, verbose=0)#x_train=y_train
 
 
-print('train_mae: ',train_mae)
-print('train_mse: ', train_mse)
-
-
-print('test_mae: ',test_mae)
-print('test_mse: ', test_mse)
-
-
-print('val_mae: ',val_mae)
-print('val_mse: ', val_mse)
